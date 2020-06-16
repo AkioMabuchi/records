831b3b1c7a6cd0a5a000fae40c8abb3d6fff72d1
AkioMabuchi <mabuchiakio@gmail.com>
2020-06-16 20:17:22 +0900

    fixed bug

--- app/controllers/unity1week16th_controller.rb
+++ app/controllers/unity1week16th_controller.rb
@@ -1,4 +1,5 @@
 class Unity1week16thController < ApplicationController
+  protect_from_forgery :except => [:send_from_unity]
   def index
     @records = Unity1week16thRecord.all
   end
@@ -6,12 +7,12 @@ class Unity1week16thController < ApplicationController
   def show
   end
 
-  def send_from_unity e
+  def send_from_unity
     record = Unity1week16thRecord.new(name: params[:name], score: params[:score])
     record.save
   end
 
-  def receive_from_unity e
+  def receive_from_unity
     records = Unity1week16thRecord.all
     render json: records
   end
