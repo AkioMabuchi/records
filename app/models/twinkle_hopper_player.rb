c7042b8a7e67114ae94a9760635a82491a6bf92a
AkioMabuchi <mabuchiakio@gmail.com>
2021-08-25 15:31:01 +0900

    fixed bug

--- app/models/twinkle_hopper_player.rb
+++ app/models/twinkle_hopper_player.rb
@@ -1,5 +1,5 @@
 class TwinkleHopperPlayer < ApplicationRecord
-  has_one :information, class_name: "TwinkleHopperInformation", dependent: :destroy
+  has_one :information, class_name: "TwinkleHopperInformation", foreign_key: :player_id, dependent: :destroy
   validates :player_token, presence: true, uniqueness: {case_sensitive: true}
   validates :player_name, presence: true
   validates :hopper_color_hue, presence: true
