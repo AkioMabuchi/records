acc98122fb8ee85266971c3932350e4b97bf364b
AkioMabuchi <mabuchiakio@gmail.com>
2022-05-06 14:24:19 +0900

    fixed bug

--- app/models/unity1week22nd_record.rb
+++ app/models/unity1week22nd_record.rb
@@ -1,5 +1,5 @@
 class Unity1week22ndRecord < ApplicationRecord
-  validates :player_id, presence: true, uniqueness: {case_sensitive: true}
+  validates :player_id, presence: true
   validates :player_name, presence: true
   validates :picture_name, presence: true
   validates :solve_time, presence: true
